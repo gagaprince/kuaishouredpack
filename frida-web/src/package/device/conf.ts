408d68cfe5004e90119837fec95c943941382b51
wangzidong <wangzidong@meituan.com>
2020-12-11 16:22:30 +0800

    feat:新增数据库支持

--- frida-web/src/package/device/conf.ts
+++ frida-web/src/package/device/conf.ts
@@ -1,5 +1,11 @@
 import { IDevice, DEVICE_STATUS } from "./device.service";
-export const DeviceList: IDevice[] = [];
+export const DeviceList: IDevice[] = [{
+    phoneNumber: '17701308857',
+    ip: 'gagalulu.wang',
+    port: 12346,
+    adbPort: 12345,
+    status: DEVICE_STATUS.OFFLINE
+}];
 
 /**
  * {
